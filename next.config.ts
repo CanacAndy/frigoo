a9d6ed12a5b3daced056615dc5a553fe3734db77
AndyCanac <a.canac@yahoo.fr>
2025-06-02 20:31:26 +0200

    [UPDATE] - preparation for prod

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Important pour Firebase
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Optionnel, si vous avez des erreurs TypeScript
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
